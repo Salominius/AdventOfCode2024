1d0d5c6ed5a025ff608dd567b37488582c5e9e39
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-01 10:31:34 +0100

    Day1

--- day01.py
+++ day01.py
@@ -3,18 +3,14 @@ from helpers.importHelpers import *
 stringInput = getInput()
 x = []
 y = []
-for line in stringInput.split("\n"):
-  x.append(int(line.split(" ")[0]))
-  y.append(int(line.split(" ")[-1]))
+for line in getInput().split("\n"):
+  x.append(int(line.split()[0]))
+  y.append(int(line.split()[1]))
 
 x.sort()
 y.sort()
 part1 = sum([abs(a - b) for a, b in zip(x, y)])
-
-part2 = 0
-for firstNum in x:
-  occ = y.count(firstNum)
-  part2 += occ * firstNum
+part2 = sum(a * y.count(a) for a in x)
 
 print("Part 1: ", part1)
 print("Part 2: ", part2)
