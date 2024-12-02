44e23930a216c3b8f1d2203f6d1e098a324a70f8
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-02 13:08:32 +0100

    Day 2 improved

--- day02.py
+++ day02.py
@@ -1,21 +1,20 @@
 from helpers.importHelpers import *
 
-stringInput = getInput()
+def isValid(levels):
+  diffs = [b-a for a, b in zip(levels[:-1], levels[1:])]
+  # minimum and maximum should not be on opposite sides of 0, and all absolute differences should be between 1 and 3
+  return not min(diffs) < 0 < max(diffs) and all(1 <= abs(diff) <= 3 for diff in diffs)
 
 part1 = 0
 part2 = 0
-for line in stringInput.split("\n"):
-  levels = [int(i) for i in line.split()]
-  diffs = [b-a for a, b in zip(levels[:-1], levels[1:])]
-
-  if not min(diffs) < 0 < max(diffs) and all(1 <= abs(diff) <= 3 for diff in diffs):
+for line in getInput().splitlines():
+  levels = [int(level) for level in line.split()]
+  if isValid(levels):
     part1 += 1
     part2 += 1
   else:
-    for index in range(len(levels)): # Performance is horrible, but idc for now
-      ignoreProb = levels[:index] + levels[index+1:]
-      diffs = [b-a for a, b in zip(ignoreProb[:-1], ignoreProb[1:])]
-      if not min(diffs) < 0 < max(diffs) and all(1 <= abs(diff) <= 3 for diff in diffs):
+    for i in range(len(levels)):
+      if isValid(levels[:i] + levels[i+1:]):
         part2 += 1
         break
 
