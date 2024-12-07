f32d9527c919e0603e5372a26dbc8e282158151e
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-07 10:47:40 +0100

    Day 7

--- day07.py
+++ day07.py
@@ -2,19 +2,24 @@ from helpers.importHelpers import *
 
 part1 = 0
 part2 = 0
-stringInput = getInput()
+
 for line in getInput().splitlines():
   result, numbers = line.split(":")
   result = int(result)
   numbers = list(map(int, numbers.split()))
+  # Part 1
   previousResults = {numbers[0]}
-  previousResults2 = {numbers[0]}
   for nextNum in numbers[1:]:
     previousResults = {result for prev in previousResults for result in (prev+nextNum, prev*nextNum)}
-    previousResults2 = previousResults | {result for prev in previousResults2 for result in (prev+nextNum, prev*nextNum, int(f'{prev}{nextNum}'))}
   if result in previousResults:
     part1 += result
-  if result in previousResults2:
+    part2 += result
+    continue
+  # Part 2
+  previousResults = {numbers[0]}
+  for nextNum in numbers[1:]:
+    previousResults = {result for prev in previousResults for result in (prev+nextNum, prev*nextNum, int(f'{prev}{nextNum}'))}
+  if result in previousResults:
     part2 += result
 
 print("Part 1: ", part1)
