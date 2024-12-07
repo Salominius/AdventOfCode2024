aafa6e57c8a9a66440f4f2889935f0d474d1c13f
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-07 10:36:54 +0100

    Arguably prettier, but not faster

--- day07.py
+++ day07.py
@@ -3,30 +3,19 @@ from helpers.importHelpers import *
 part1 = 0
 part2 = 0
 stringInput = getInput()
-for line in stringInput.splitlines():
+for line in getInput().splitlines():
   result, numbers = line.split(":")
+  result = int(result)
   numbers = list(map(int, numbers.split()))
-  previousResults = [[numbers[0]]]
-  for i in range(len(numbers)-1):
-    previousResults.append([])
-    for j in range(len(previousResults[i])):
-      previousResults[i+1].append(previousResults[i][j] + numbers[i+1])
-      previousResults[i+1].append(previousResults[i][j] * numbers[i+1])
-  if int(result) in previousResults[-1]:
-    part1 += int(result)
-
-for line in stringInput.splitlines():
-  result, numbers = line.split(":")
-  numbers = list(map(int, numbers.split()))
-  previousResults = [[numbers[0]]]
-  for i in range(len(numbers)-1):
-    previousResults.append([])
-    for j in range(len(previousResults[i])):
-      previousResults[i+1].append(previousResults[i][j] + numbers[i+1])
-      previousResults[i+1].append(previousResults[i][j] * numbers[i+1])
-      previousResults[i+1].append(int(str(previousResults[i][j]) + str(numbers[i+1])))
-  if int(result) in previousResults[-1]:
-    part2 += int(result)
+  previousResults = {numbers[0]}
+  previousResults2 = {numbers[0]}
+  for nextNum in numbers[1:]:
+    previousResults = {result for prev in previousResults for result in (prev+nextNum, prev*nextNum)}
+    previousResults2 = previousResults | {result for prev in previousResults2 for result in (prev+nextNum, prev*nextNum, int(f'{prev}{nextNum}'))}
+  if result in previousResults:
+    part1 += result
+  if result in previousResults2:
+    part2 += result
 
 print("Part 1: ", part1)
 print("Part 2: ", part2)
