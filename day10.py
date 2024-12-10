942756b0409e3d3d1c0442d8ea92d86a601156b6
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-10 11:41:52 +0100

    Day 10

--- day10.py
+++ day10.py
@@ -6,32 +6,32 @@ def inBounds(x, y, bounds):
 grid = [list(map(int, line)) for line in getInput().splitlines()]
 bounds = (len(grid[0]), len(grid))
 
-reachable1 = [[set() for _ in range(bounds[0])] for _ in range(bounds[1])]
-reachable2 = [[0 for _ in range(bounds[0])] for _ in range(bounds[1])]
+reachableEnds = [[set() for _ in range(bounds[0])] for _ in range(bounds[1])] # For each grid-cell, the set of reachable ends
+numPathsToEnds = [[0 for _ in range(bounds[0])] for _ in range(bounds[1])] # For each grid-cell, the number of paths to reachable ends
 
 part1 = 0
 part2 = 0
 for y in range(bounds[1]):
   for x in range(bounds[0]):
     if grid[y][x] == 9:
-      reachable1[y][x] = set([(x, y)])
-      reachable2[y][x] = 1
+      reachableEnds[y][x] = set([(x, y)]) # The end is reachable from itself
+      numPathsToEnds[y][x] = 1 # There is one path from the end to the end
 
-for i in range(9, 0, -1):
+for i in range(9, 0, -1): # For each number from 9 to 1
   for y in range(bounds[1]):
     for x in range(bounds[0]):
-      if grid[y][x] == i:
-        for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+      if grid[y][x] == i: # If the cell contains the current number
+        for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]: # Go through neighbours
           newX, newY = x + direction[0], y + direction[1]
-          if inBounds(newX, newY, bounds) and grid[newY][newX] == i - 1:
-            reachable1[newY][newX] |= reachable1[y][x]
-            reachable2[newY][newX] += reachable2[y][x]
+          if inBounds(newX, newY, bounds) and grid[newY][newX] == i - 1: # If neighbour can reach the current cell
+            reachableEnds[newY][newX] |= reachableEnds[y][x] # All current reachable ends can be reached by the neighbour
+            numPathsToEnds[newY][newX] += numPathsToEnds[y][x] # All current paths can be reached by the neighbour
 
 for y in range(bounds[1]):
   for x in range(bounds[0]):
-    if grid[y][x] == 0:
-      part1 += len(reachable1[y][x])
-      part2 += reachable2[y][x]
+    if grid[y][x] == 0: # For each trail head
+      part1 += len(reachableEnds[y][x]) # Count the number of reachable ends
+      part2 += numPathsToEnds[y][x] # Count the number of paths to reachable ends
 
 print("Part 1: ", part1)
 print("Part 2: ", part2)
