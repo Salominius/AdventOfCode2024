23c944e83e53a53e08b7e187a277948d8ca52762
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-11 10:16:44 +0100

    Day 11

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-![](https://img.shields.io/badge/stars%20⭐-20-yellow)
-![](https://img.shields.io/badge/days%20completed-10-blue)
+![](https://img.shields.io/badge/stars%20⭐-22-yellow)
+![](https://img.shields.io/badge/days%20completed-11-blue)
 # AdventOfCode2024
 My approach on Advent Of Code this year using Python
 
--- day11.py
+++ day11.py
@@ -1,18 +1,26 @@
 from helpers.importHelpers import *
 
-stones = getInput().split()
+stones = {stone: 1 for stone in getInput().split()}
+
+def step(stones):
+  nextState = {}
+  for stone, num in stones.items():
+    if stone == '0':
+      nextState['1'] = nextState.setdefault('1', 0) + num
+    elif len(stone) % 2 == 0:
+      leftHalf, rightHalf = stone[:len(stone)//2], str(int(stone[len(stone)//2:]))
+      nextState[leftHalf] = nextState.setdefault(leftHalf, 0) + num
+      nextState[rightHalf] = nextState.setdefault(rightHalf, 0) + num
+    else:
+      nextState[str(int(stone)*2024)] = nextState.setdefault(str(int(stone)*2024), 0) + num
+  return nextState
 
 for i in range(25):
-  nextIt = []
-  for stone in stones:
-    if stone == '0':
-      nextIt.append('1')
-    elif len(stone) % 2 == 0:
-      nextIt.append(stone[:len(stone)//2])
-      nextIt.append(str(int(stone[len(stone)//2:])))
-    else:
-      nextIt.append(str(int(stone)*2024))
-  stones = nextIt
+  stones = step(stones)
+part1 = sum(stones.values())
+for i in range(50):
+  stones = step(stones)
+part2 = sum(stones.values())
 
-print("Part 1: ", len(stones))
-print("Part 2: ", 0)
+print("Part 1: ", part1)
+print("Part 2: ", part2)
