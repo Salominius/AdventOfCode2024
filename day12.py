39b53366eff6cff631887d2161562be529c9302b
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-12 12:06:45 +0100

    Day 12

--- day12.py
+++ day12.py
@@ -1,16 +1,20 @@
 from helpers.importHelpers import *
 
+plantID = 0
 garden = [list(line) for line in getInput().splitlines()]
-covered = [[False]*len(garden) for _ in range(len(garden))] # garden is a square
+fields = {} # plantID: (area, perimeter, sides)
+
+def inBounds(x, y):
+  return 0 <= x < len(garden[0]) and 0 <= y < len(garden) # garden has same width and height
 
 def floodill(plant, x, y):
-  if x < 0 or x >= len(garden[0]) or y < 0 or y >= len(garden):
+  if not inBounds(x, y):
     return 0, 1 # out of bounds
+  if garden[y][x] == plantID:
+    return 0, 0 # already covered
   if garden[y][x] != plant:
     return 0, 1 # different plant
-  if covered[y][x]:
-    return 0, 0 # already covered
-  covered[y][x] = True
+  garden[y][x] = plantID
   area, perimeter = 1, 0
   for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
     a, p = floodill(plant, x + dx, y + dy)
@@ -18,12 +22,24 @@ def floodill(plant, x, y):
     perimeter += p
   return area, perimeter
 
-part1 = 0
 for y in range(len(garden)):
   for x in range(len(garden[0])):
-    if not covered[y][x]:
+    if not isinstance(garden[y][x], int):
       area, perimeter = floodill(garden[y][x], x, y)
-      part1 += area*perimeter
+      fields[plantID] = (area, perimeter, perimeter) # perimeter is default for number of sides. reduced later
+      plantID += 1
 
-print("Part 1: ", part1)
-print("Part 2: ", 0)
+for y in range(len(garden)):
+  for x in range(len(garden[0])):
+    currentPlant = garden[y][x] # for each cell
+    for movex, movey in [(1, 0), (0, 1)]: # move right and down
+      if not inBounds(x+movex, y+movey) or garden[y+movey][x+movex] != currentPlant: # if leaving the field, nothing happens
+        continue
+      # If next cell is part of same field, check sides of originial cell and next cell.
+      for sidex, sidey in [(movey, movex), (-movey, -movex)]: # sides are calculated with offset perpendicular to the direction of movement
+        if not inBounds(x+sidex, y+sidey) or (currentPlant != garden[y+sidey][x+sidex] and currentPlant != garden[y+movey+sidey][x+movex+ sidex]):
+          # if side of previous cell and side of next cell are both not part of field, number of sides is reduced by one
+          fields[currentPlant] = (fields[currentPlant][0], fields[currentPlant][1], fields[currentPlant][2] - 1)
+
+print("Part 1: ", sum(area*perimeter for area, perimeter, _ in fields.values()))
+print("Part 2: ", sum(area*sides for area, _, sides in fields.values()))
