bf5a24d407b1ade622759745596c104121fb8ac1
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-14 11:20:29 +0100

    Day 13 moved re compile outside of loop

--- day13.py
+++ day13.py
@@ -24,13 +24,13 @@ def findWinningCost(x_a, y_a, x_b, y_b, x_p, y_p):
   a = (y_b*x_p - x_b*y_p)/(x_a*y_b - x_b*y_a)
   b = (-y_a*x_p + x_a*y_p)/(x_a*y_b - x_b*y_a)
   if a.is_integer() and b.is_integer():
-    return int(abs(a)*3 + abs(b))
+    return int(3*a + b)
   return 0
 
 part1 = 0
 part2 = 0
+pattern = re.compile(r"X[=|+]?(\d+), Y[=|+]?(\d+)")
 for machine in getInput().split("\n\n"):
-  pattern = re.compile(r"X[=|+]?(\d+), Y[=|+]?(\d+)")
   a, b, prize = map(lambda t: (int(t[0]), int(t[1])), pattern.findall(machine)) # don't forget to map tuples of string-matches to tuples of integers
   part1 += findWinningCost(*a, *b, *prize)
   part2 += findWinningCost(*a, *b, prize[0]+1e13, prize[1]+1e13)
