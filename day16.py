397f279bc48a762edc296c4fdcaaa4954eb6d86a
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-16 11:42:52 +0100

    Day 16

--- day16.py
+++ day16.py
@@ -11,38 +11,35 @@ for j, row in enumerate(getInput().splitlines()):
     elif c == "E":
       end = i + j*1j
 
-# Part 1:
-queue = set([(start, 0, 1)],)
-points = {start: {1: 0}}
+directions = [1, -1j, -1, 1j]
+def getTurnCost(oldDir, newDir):
+  rightTurns = (directions.index(newDir) - directions.index(oldDir)) % 4
+  leftTurns = (4 - rightTurns) % 4
+  return min(leftTurns, rightTurns)*1000
 
+# Part 1:
+queue = {(start, 1, 0)} # (position, direction, points)
+points = {start: {1: 0}} # for each coordinate, store a dict with directions as keys and points as values
 while queue:
-  pos, oldPoints, oldDir = queue.pop()
-  directions = [1, -1j, -1, 1j]
+  pos, oldDir, oldPoints = queue.pop()
   for newDir in directions:
-    newPos = pos + newDir
-    if newPos in walls:
+    if (newPos := pos+newDir) in walls:
       continue
-    rightTurns = (directions.index(newDir) - directions.index(oldDir)) % 4
-    leftTurns = (4 - rightTurns) % 4
-    points[pos][newDir] = min(points[pos].get(newDir, float("inf")), oldPoints + min(leftTurns, rightTurns)*1000)
+    points[pos][newDir] = min(points[pos].get(newDir, float("inf")), oldPoints + getTurnCost(oldDir, newDir))
     newPoints = points[pos][newDir] + 1
     if newPos not in points or points[newPos].get(newDir, float("inf")) > newPoints:
       points.setdefault(newPos, {})[newDir] = newPoints
-      queue.add((newPos, newPoints, newDir))
-  
+      queue.add((newPos, newDir, newPoints))
 part1 = min(points[end].values())
 
 # Part 2:
-part2 = set([end],)
-queue = set([(end, *next((key, value) for key, value in points[end].items() if value == part1))],)
+part2 = {end}
+queue = {(end, min(points[end], key=points[end].get), part1)} # (position, direction, points)
 while queue:
   pos, oldDir, oldPoints = queue.pop()
-  directions = [1, -1j, -1, 1j]
   for newDir in directions:
     newPos = pos - newDir
-    rightTurns = (directions.index(newDir) - directions.index(oldDir)) % 4
-    leftTurns = (4 - rightTurns) % 4
-    if newPos in points and points[newPos][newDir] == oldPoints - 1 - min(leftTurns, rightTurns)*1000:
+    if newPos in points and points[newPos][newDir] == oldPoints - 1 - getTurnCost(newDir, oldDir):
       part2.add(newPos)
       queue.add((newPos, newDir, points[newPos][newDir]))
 
