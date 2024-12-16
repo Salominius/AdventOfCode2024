67deaf3b41b91382277373d42190639dfc35f96a
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-16 15:42:43 +0100

    Day 16 added comments

--- day16.py
+++ day16.py
@@ -25,23 +25,27 @@ while queue:
   for newDir in directions:
     if (newPos := pos+newDir) in walls:
       continue
+    # Update cost to look in newDir at current position:
     points[pos][newDir] = min(points[pos].get(newDir, float("inf")), oldPoints + getTurnCost(oldDir, newDir))
+    # Cost to move in newDir from current position: 
     newPoints = points[pos][newDir] + 1
+    # If we reach a point we havn't reached before, or if we reach it cheaper then before, store new cost and add point to queue:
     if newPos not in points or points[newPos].get(newDir, float("inf")) > newPoints:
       points.setdefault(newPos, {})[newDir] = newPoints
       queue.add((newPos, newDir, newPoints))
 part1 = min(points[end].values())
 
 # Part 2:
-part2 = {end}
+part2 = {end} # set of all positions that are part of an optimal path
 queue = {(end, min(points[end], key=points[end].get), part1)} # (position, direction, points)
 while queue:
-  pos, oldDir, oldPoints = queue.pop()
-  for newDir in directions:
-    newPos = pos - newDir
-    if newPos in points and points[newPos][newDir] == oldPoints - 1 - getTurnCost(newDir, oldDir):
-      part2.add(newPos)
-      queue.add((newPos, newDir, points[newPos][newDir]))
+  pos, curDir, curPoints = queue.pop()
+  for prevDir in directions:
+    prevPos = pos - prevDir # walk backwards
+    # If currentPos, direction and cost can be achieved from prevPos, prevPos must be part of an optimal path 
+    if prevPos in points and points[prevPos][prevDir] == curPoints - 1 - getTurnCost(prevDir, curDir):
+      part2.add(prevPos)
+      queue.add((prevPos, prevDir, points[prevPos][prevDir]))
 
 print("Part 1: ", part1)
 print("Part 2: ", len(part2))
