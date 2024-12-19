750a96d061470ca3a388bca8b6bc7c7141f114f0
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-19 08:34:09 +0100

    Day 19 using cache

--- day19.py
+++ day19.py
@@ -6,24 +6,14 @@ towels = towels.split(", ")
 patterns = patterns.split("\n")
 
 @lru_cache(maxsize=None)
-def part1(pattern):
-  if pattern == "":
-    return 1
-  for towel in towels:
-    if pattern.startswith(towel):
-      if part1(pattern[len(towel):]):
-        return 1
-  return 0
-
-@lru_cache(maxsize=None)
-def part2(pattern):
+def checkPattern(pattern):
   if pattern == "":
     return 1
   res = 0
   for towel in towels:
     if pattern.startswith(towel):
-      res += part2(pattern[len(towel):])
+      res += checkPattern(pattern[len(towel):])
   return res
 
-print("Part 1: ", sum([part1(pattern) for pattern in patterns]))
-print("Part 2: ", sum([part2(pattern) for pattern in patterns]))
+print("Part 1: ", sum([checkPattern(pattern)>0 for pattern in patterns]))
+print("Part 2: ", sum([checkPattern(pattern) for pattern in patterns]))
