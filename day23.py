85a63988a351abffa3eff1e8243cbd214d69e911
Arne Malaka <a12028537@unet.univie.ac.at>
2024-12-23 23:41:54 +0100

    Day 23 with good Performance

--- day23.py
+++ day23.py
@@ -6,16 +6,27 @@ for line in getInput().splitlines():
   connections.setdefault(a, set()).add(b)
   connections.setdefault(b, set()).add(a)
 
-components = set()
+# Part 1 separately for better performance:
+trippleComponents = set()
 for a, bList in connections.items():
-  maxSize = max([len(group) for group in components], default=0)
+  for b in bList:
+    if b > a:
+      for c in connections[b]:
+        if c > b and c in bList and any([x[0] == "t" for x in [a, b, c]]):
+          trippleComponents.add((a, b, c))
+
+# Part 2 (Find biggest connected component):
+biggestComponent = tuple()
+for a, bList in connections.items():
   aGroups = [{a} | bList]
   for b in bList:
     withB = [group.intersection(connections[b] | {b}) for group in aGroups]
     withoutB = [group - {b} for group in aGroups]
     aGroups = withB + withoutB
+    aGroups = [g for g in aGroups if len(g) > len(biggestComponent)]
   for group in aGroups:
-    components.add(tuple(sorted(group)))
+    if len(group) > len(biggestComponent):
+      biggestComponent = sorted(group)
 
-print("Part 1: ", sum([1 for group in components if len(group) == 3 and any([x[0] == "t" for x in group])]))
-print("Part 2: ", ",".join(sorted(max(components, key=len))))
+print("Part 1: ", len(trippleComponents))
+print("Part 2: ", ",".join(biggestComponent))
